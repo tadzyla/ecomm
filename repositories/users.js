1f0366b1842e93d67bee887fed0da4fa13266b09
tadzyla <tadas.zvinklys@gmail.com>
2021-02-09 11:55:42 +0000

    reading file async

--- repositories/users.js
+++ repositories/users.js
@@ -1,18 +1,41 @@
 const fs = require('fs');
 
-class usersRepository {
+class UsersRepository {
     constructor(filename) {
-if(!filename) {
-    throw new Error('Creating a repository requies a filename');
+        if(!filename) {
+            throw new Error('Creating a repository requies a filename');
         }
-    this.filename = filename;
-    try {
-    fs.accessSync(this.filename);
-    } catch(err){
-        fs.writeFileSync(this.filename, '[]');
+        this.filename = filename;
+        try {
+            fs.accessSync(this.filename);
+        } catch(err){
+            fs.writeFileSync(this.filename, '[]');
+        }
+    }
+
+    async getAll(){
+        return new Promise((resolve, reject) => {
+            fs.readFile(this.filename, 'utf-8', (err, data) => {
+                if (err) reject(err);
+                else resolve (JSON.parse(data))
+            })
+        })
     }
 }
-}
+   
 
-const repo = new usersRepository('users.json'); 
+const test = async () => {
+    const repo = new UsersRepository('users.json'); 
+    const users = await repo.getAll()
+        .then((val) => {
+            console.log('rezultatas: ', val);
+            return val;
+        })
+        .catch((err) => {
+            console.log('blogai', err)
+        })
 
+    console.log('users are: ', users);
+}; 
+
+test ();
