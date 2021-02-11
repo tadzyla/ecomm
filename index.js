f61f2dd8d76987581fefec28c59ed825d4d6a462
tadzyla <tadas.zvinklys@gmail.com>
2021-02-11 23:02:53 +0000

    sing in & sing out methods
    Signed-off-by: tadzyla <tadas.zvinklys@gmail.com>

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.get('/signup', (req, res) => {  //someone makes a network request, callback 
         <input name="email" placeholder="email" />
         <input name="password" placeholder="password" />
         <input name="passwordConfirmation" placeholder="password confirmation" />
-        <button>sign up</button>
+        <button>Sign up</button>
         </form>
     </div>
     `);
@@ -46,7 +46,37 @@ app.post('/signup', async (req, res) => {
     res.send('Account created!');
 });
 
+app.get('/signout', (req, res) => {
+    req.session = null;
+    res.send('You are loged out');
+});
 
+app.get('/signin', (req, res) => {
+    res.send(`
+    <div>
+        <form method="POST">
+        <input name="email" placeholder="email" />
+        <input name="password" placeholder="password" />
+        <button>Sign in</button>
+        </form>
+    </div>
+    `
+    )
+})
+
+app.post('/signin', async (req, res) => {
+    const { email, password } = req.body;
+    const user = await usersRepo.getOneBy({email});
+
+    if(!user) {
+        return res.send('Email is not found');
+    }
+    if(user.password !== password){
+        return res.send('Password is wrong');
+    }
+    req.session.userId = user.id;
+    res.send('You are signed in!');
+});
 
 app.listen(8080, () => {
     console.log('Listen');
