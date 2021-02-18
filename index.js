051f6fdaf33f1c36bad8e27872a7712c7b45d1cb
tadzyla <tadas.zvinklys@gmail.com>
2021-02-18 19:15:03 +0000

    adding styling
    Signed-off-by: tadzyla <tadas.zvinklys@gmail.com>

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
 const authRouter = require('./routes/admin/auth');
+
 const app = express();
 
-
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true})); //middleware 
 app.use(
     cookieSession({
