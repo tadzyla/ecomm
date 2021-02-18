bd93d22840ce50120b06d7b69061c8f83f66446f
tadzyla <tadas.zvinklys@gmail.com>
2021-02-18 23:19:57 +0000

    adding product validator
    Signed-off-by: tadzyla <tadas.zvinklys@gmail.com>

--- views/admin/products/new.js
+++ views/admin/products/new.js
@@ -1,5 +1,5 @@
 const layout = require('../layout');
-const getError = require('../../helpers');
+const { getError } = require('../../helpers');
 
 module.exports = ({ errors }) => {
     return layout({
